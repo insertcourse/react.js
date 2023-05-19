58f699f186dd5faaf75deb0d2983344c616334e2
youngeunkim <pkhj286@gmail.com>
2023-05-19 23:40:40 +0900

    ADD : Day 1

---
+++ DAY1/mission/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+
+const App = () => (
+    <div className="container">
+    <h1 className="title">나는 김영은</h1>
+    <p className="content">이건 내용입니돠</p>
+    <button className="btn">이건 버튼이지롱</button>
+    <span className="span">감사합니다...</span>
+    </div>
+);
+
+export default App;
