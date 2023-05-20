7dfe7db41737498530d64bad768ee7da5415538b
youngeunkim <pkhj286@gmail.com>
2023-05-21 00:21:39 +0900

    ADD : Day 4

---
+++ DAY4/mission/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Introduce from './Introduce';
+
+const App = () => {
+    return (
+        <div>
+            <h1 style = {{color:'pink'}}>자기소개</h1>
+            <Introduce />
+        </div>
+);
+};
+
+export default App;
