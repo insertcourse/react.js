88d209ab48d75740cc617384d3f9af80861d5027
kimsiyeon0223 <Ksyeon0223@gmail.com>
2023-05-20 23:56:34 +0900

    DAY7 ADD : 헤더

---
+++ DAY7/todolist/Header/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <div className='todo'>My Todolist</div>
+        </div>
+    );
+};
+
+export default Header;
+
+
+
+
+
