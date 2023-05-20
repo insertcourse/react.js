1aa2880d71332860be2c865f64ecc7d7b16cfd99
kimsiyeon0223 <Ksyeon0223@gmail.com>
2023-05-20 23:57:00 +0900

    DAY7 ADD : 투두리스트

---
+++ DAY7/todolist/TodoList/TodoList.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import TodoListItem from '../../../DAY7/todolist/TodoListItem/TodoListItem';
+import Header from '../../../DAY7/todolist/Header/Header';
+
+
+const TodoList = () => {
+    return (
+        <div>
+            <Header/>
+            <TodoListItem/>
+        </div>
+    );
+};
+
+export default TodoList;
