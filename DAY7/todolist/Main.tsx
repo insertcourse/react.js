610bc3df5f7206a62def7651d1cbabfec7056369
kimsiyeon0223 <Ksyeon0223@gmail.com>
2023-05-20 23:55:53 +0900

    DAY7 ADD : 메인페이지

---
+++ DAY7/todolist/Main.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div>
+            <div>
+                <h1>시연이의 투두리스트</h1>
+                <h3>todolist란? 해야 할 작업을 모두 정리한 목록을 말합니다. 하는 것을 알기 쉽게 시각화하여 작업의 우선순위를 매기고 시간 관리를 쉽게 하기 위한 목록입니다.</h3>
+            </div>
+        </div>
+    );
+};
+
+export default Main;
