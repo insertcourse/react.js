9b1b4f46752d5294a41bae55f4db8db6218a49a1
youngeunkim <pkhj286@gmail.com>
2023-05-21 11:44:04 +0900

    ADD :

--- DAY7/todolist/TodoList/TodoList.tsx
+++ DAY7/todolist/TodoList/TodoList.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import TodoListItem from './TodoListItem';
 import Header from '.Header';
+import AddToDo from '.AddTodo'
+
 
 const TodoList = ({ todoes }) => {
     return (
         <div>
             <Header />
+            <AddToDo />
             {todoes.map((todo) => (
             <TodoListItem todo={todo} />
 ))}
