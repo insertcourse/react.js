a15e86dc099bf6ef87e8e201153fc0eae38c1a23
youngeunkim <pkhj286@gmail.com>
2023-05-21 10:13:26 +0900

    FIX : Day 6 header import

--- DAY6/todolist/TodoList/TodoList.js
+++ DAY6/todolist/TodoList/TodoList.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import TodoListItem from './TodoListItem';
+import Header from '.Header';
 
 const TodoList = ({ todoes }) => {
     return (
         <div>
+            <Header />
             {todoes.map((todo) => (
             <TodoListItem todo={todo} />
 ))}
