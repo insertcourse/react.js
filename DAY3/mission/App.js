2833ba65a8958f232f54a82378a628097516ad27
youngeunkim <pkhj286@gmail.com>
2023-05-26 17:18:36 +0900

    REFACTOR : 스타일 지정

--- DAY3/mission/App.js
+++ DAY3/mission/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import List from "./List";
+import "App.css";
 
 const App = () => {
   return (
     <div>
-      <h1 style={{ color: "blue" }}>내가 좋아하는 과일 리스트</h1>
+      <h1>내가 좋아하는 과일 리스트</h1>
       <List />
     </div>
   );
