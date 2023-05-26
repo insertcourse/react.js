b27b6e863df219c86b8a4d79ff295b84088c84c8
youngeunkim <pkhj286@gmail.com>
2023-05-26 19:28:03 +0900

    REFECTOR : br 태그 수정

--- DAY3/mission/Greeting.js
+++ DAY3/mission/Greeting.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Greeting.css";
 
 const Greeting = () => {
   const [message, setMessage] = React.useState("안녕하세요!");
@@ -13,26 +14,25 @@ const Greeting = () => {
   };
 
   const colorChange = (newColor) => {
-    //버튼 눌렀을 때 색을 변경하는
     setColor(newColor);
   };
 
   return (
-    <div>
+    <div className="greeting--message">
       <button onClick={enter}>입장</button>
       <button onClick={exit}>퇴장</button>
-      <br />
-      <span style={{ color }}>{message}</span>
-      <br />
-      <button style={{ color: "red" }} onClick={colorChange("red")}>
-        빨간색
-      </button>
-      <button style={{ color: "green" }} onClick={colorChange("green")}>
-        초록색
-      </button>
-      <button style={{ color: "blue" }} onClick={colorChange("blue")}>
-        파란색
-      </button>
+      <div className="button--color">
+        <span style={{ color }}>{message}</span>
+        <button style={{ color: "red" }} onClick={() => colorChange("red")}>
+          빨간색
+        </button>
+        <button style={{ color: "green" }} onClick={() => colorChange("green")}>
+          초록색
+        </button>
+        <button style={{ color: "blue" }} onClick={() => colorChange("blue")}>
+          파란색
+        </button>
+      </div>
     </div>
   );
 };
