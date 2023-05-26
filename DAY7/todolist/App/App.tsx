fb68ea9258429e142246e376d7cb3583cfe3f640
youngeunkim <pkhj286@gmail.com>
2023-05-26 17:05:27 +0900

    FIX : path 소문자 사용

--- DAY7/todolist/App/App.tsx
+++ DAY7/todolist/App/App.tsx
@@ -1,16 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; //router
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import TodoList from "./TodoList";
 
 const App = () => {
   return (
     <div>
-      <Router>
-        <Routes>
-          <Route path="/"></Route>
-          <Route path="/Todolist" element={<TodoList />}></Route>
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/"></Route>
+        <Route path="/todolist" element={<TodoList />}></Route>
+      </Routes>
       <h1>안녕하세요</h1>
       <p>부산 소프트웨어 마이스터고를 다니고 있는 김영은입니돠...</p>
       <p>투두리스트를 만들어 보았습니다. 하하 </p>
