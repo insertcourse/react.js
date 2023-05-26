bcb36b5359743e0f8b4236df60edabe7e8e7b98e
youngeunkim <pkhj286@gmail.com>
2023-05-26 16:51:13 +0900

    FIX : 경로 표시

--- DAY6/todolist/TodoList/TodoList.js
+++ DAY6/todolist/TodoList/TodoList.js
@@ -1,7 +1,7 @@
 import React from "react";
 import TodoListItem from "./TodoListItem";
-import Header from ".Header";
-import AddToDo from ".AddToDo";
+import Header from "./Header";
+import AddToDo from "./AddToDo";
 
 const TodoList = ({ todoes }) => {
   return (
